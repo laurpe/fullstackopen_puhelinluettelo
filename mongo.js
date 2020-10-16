f9b59f6781778c5623cff81b181dcdd749e1f18d
Laura Perttilä <laura@perttila.me>
2020-10-16 12:38:59 +0300

    tehtävä 3.12

---
+++ mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://laura:${password}@cluster0.ahtgt.mongodb.net/puhelinluettelo-app?retryWrites=true&w=majority`;
+
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+if (process.argv.length === 3) {
+  console.log("Phonebook:");
+  Person.find({}).then((result) => {
+    result.forEach((person) => {
+      console.log(`${person.name} ${person.number}`);
+    });
+    mongoose.connection.close();
+  });
+}
+
+if (process.argv.length === 5) {
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+  });
+
+  person.save().then((response) => {
+    console.log(
+      `added ${person.name} number ${person.number} to the phonebook`
+    );
+    mongoose.connection.close();
+  });
+}
